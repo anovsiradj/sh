f9a38b11d1511e6501141f2bc73d72e0807848ef
anovsiradj <anov.siradj22@gmail.com>
2021-06-13 09:32:28 +0700

    accept pwd param

--- utilities/password-bcrypt.php
+++ utilities/password-bcrypt.php
@@ -1,8 +1,12 @@
 <?php
 
 $pwd = 'pass';
+if (isset($argv) && array_key_exists(1, $argv)) {
+	$pwd = $argv[1];
+}
+
 $pwd_options['cost'] = rand(4,12);
 
-$result = password_hash($pwd, PASSWORD_BCRYPT, $pwd_options);
+$pwd_result = password_hash($pwd, PASSWORD_BCRYPT, $pwd_options);
 
-echo PHP_EOL, $result, PHP_EOL, PHP_EOL;
+echo PHP_EOL, $pwd, PHP_EOL, $pwd_result, PHP_EOL, PHP_EOL;
